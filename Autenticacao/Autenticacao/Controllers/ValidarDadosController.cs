4f37aeecad3898b59301e65b6dae6b82e142d3cb
Luiz Barros <figueiredobarros@gmail.com>
2019-04-16 20:04:34 -0300

    api autenticacao

--- Autenticacao/Autenticacao/Controllers/ValidarDadosController.cs
+++ Autenticacao/Autenticacao/Controllers/ValidarDadosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autenticacao.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +12,9 @@ namespace Autenticacao.Controllers
     [ApiController]
     public class ValidarDadosController : ControllerBase
     {
-        [HttpPost]
+        [HttpGet]
         [Route("usuario")]
-        public ActionResult<IEnumerable<string>> AutenticarUsuario([FromBody] string livroJson)
+        public ActionResult<IEnumerable<string>> AutenticarUsuario([FromQuery] Usuario usuario)
         {
             return Ok();
         }
--- Autenticacao/Autenticacao/Model/Usuario.cs
+++ Autenticacao/Autenticacao/Model/Usuario.cs
@@ -7,5 +7,8 @@ namespace Autenticacao.Model
 {
     public class Usuario
     {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+
     }
 }
